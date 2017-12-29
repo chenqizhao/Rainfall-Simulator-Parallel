3c03d2b59318d2befd48e5e7bb2b4e76229428dd
ChenqiZhao <zhaochenqi2013@gmail.com>
2017-12-29 16:18:24 -0500

    project uploaded

---
+++ rainfall.cpp
@@ -0,0 +1,274 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <time.h>
+
+int N; //the input gridSize
+int t = 1;
+int rain_time;
+double absorb_rate;
+
+std::vector<std::vector<int>> land; //landscape
+std::vector<std::vector<double> > trap_water;
+std::vector<std::vector<std::string> > lowest_neighbours; //lowestNeighbors
+std::vector<std::vector<double> > absorbed; //absorbed rainfall
+std::vector<std::vector<double> > flux; // trickels flow to neighbors
+
+void init() {
+  land = std::vector<std::vector<int> > (N, std::vector<int>(N, 0));
+  trap_water = std::vector<std::vector<double> > (N, std::vector<double>(N, 0));
+  lowest_neighbours = std::vector<std::vector<std::string> > (N, std::vector<std::string>(N, ""));
+  absorbed = std::vector<std::vector<double> > (N, std::vector<double>(N, 0));
+  flux = std::vector<std::vector<double> > (N, std::vector<double>(N, 0));
+}
+
+bool dry() {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      if (trap_water[i][j] != 0) {
+	return false;
+      }
+    }
+  }
+  return true;
+}
+bool wet() {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      if (trap_water[i][j] != 0) {
+	return true;
+      }
+    }
+  }
+  return false;
+}
+
+std::vector<int> get_neighbors_heights(int row, int col) {
+  int u = 0;
+  int d = 0;
+  int r = 0;
+  int l = 0;
+
+  if (row == 0) {
+    u = -1;
+    d = land[row + 1][col];
+  } else if (row == N - 1) {
+    u = land[row - 1][col];
+    d = -1;
+  } else {
+    u = land[row - 1][col];
+    d = land[row + 1][col];
+  }
+
+  if (col == 0) {
+    l = -1;
+    r = land[row][col + 1];
+  } else if (col == N - 1) {
+    l = land[row][col - 1];
+    r = -1;
+  } else {
+    l = land[row][col - 1];
+    r = land[row][col + 1];
+  }
+    
+  std::vector<int> ret;
+  ret.push_back(u);
+  ret.push_back(d);
+  ret.push_back(l);
+  ret.push_back(r);
+  return ret;
+}
+
+std::vector<int> find_direction(char ch) {
+  int dx = 0;
+  int dy = 0;
+  if (ch == 'u') {
+    dx = -1;
+    dy = 0;
+  } else if (ch == 'd') {
+    dx = 1;
+    dy = 0;
+  } else if (ch == 'l') {
+    dx = 0;
+    dy = -1;
+  } else if (ch == 'r') {
+    dx = 0;
+    dy = 1;
+  }
+  std::vector<int> ret;
+  ret.push_back(dx);
+  ret.push_back(dy);
+  return ret;
+}
+
+std::string htos(int k) {
+  std::string ret = "";
+  if (k == 0) {
+    ret = "u";
+  } else if (k == 1) {
+    ret = "d";
+  } else if (k == 2) {
+    ret = "l";
+  } else {
+    ret = "r";
+  }
+  return ret;
+}
+
+void init_lowest() {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      int lowest = land[i][j];
+      std::vector<int> heights = get_neighbors_heights(i, j);
+      for (int h : heights) {
+	if (h != -1 && h < lowest) {
+	  lowest = h;
+	}
+      }
+      std::string ret = "";
+      if (lowest != land[i][j]) {
+	for (int k = 0; k < 4; k++) {
+	  if (heights[k] >= 0 && heights[k] == lowest) {
+	    ret += htos(k);
+	  }
+	}
+      }
+      lowest_neighbours[i][j] = ret;
+    }
+  }
+}
+
+void calc_flux(int row, int col) {
+  std::string lowest = lowest_neighbours[row][col];
+  double flux_vol = 1.0;
+  if (trap_water[row][col] != 0 && (lowest.length() != 0)) {
+    if (trap_water[row][col] < flux_vol) {
+      flux_vol = trap_water[row][col];
+    }
+  } else {
+    flux_vol = 0.0;
+  }
+  flux[row][col] = flux_vol;
+} 
+
+void water_flow(int row, int col) {
+  std::string lowest = lowest_neighbours[row][col];
+  if (trap_water[row][col] > 0 && lowest.length() != 0) {
+    trap_water[row][col] -= flux[row][col];
+    double num = (double) lowest.length();
+    for (int n = 0; n < lowest.length(); n++) {
+      char direction = lowest.at(n);
+      std::vector<int> dir = find_direction(direction);
+      int dx = dir[0];
+      int dy = dir[1];
+      trap_water[row + dx][col + dy] += flux[row][col] / num;
+    }
+  }
+}
+
+int rain(int t, int rain_time, int timestep) {
+  while (t <= rain_time || !dry()) {
+    for (int row = 0; row < N; ++row) {
+      for (int col = 0; col < N; ++col) {
+        if (t <= rain_time) {
+	  trap_water[row][col] += 1;
+	}
+        double water = trap_water[row][col];
+        if (water > 0) {
+	  double remaining = water - absorb_rate;
+	  if (remaining < 0) {
+	    absorbed[row][col] += water;
+	    trap_water[row][col] = 0;
+	  } else {
+	    absorbed[row][col] += absorb_rate;
+	    trap_water[row][col] = remaining;
+	  }
+	}
+        calc_flux(row, col);
+      }
+    }
+    for (int row = 0; row < N; ++row) {
+      for (int col = 0; col < N; ++col) {
+        water_flow(row, col);
+      }
+    }
+    if (rain_time >= t) {
+      ++t;
+    }
+    ++timestep;
+  }
+  return timestep;
+}
+
+double calc_time(struct timespec start, struct timespec end) {
+  double start_sec = (double)start.tv_sec*1000000000.0 + (double)start.tv_nsec;
+  double end_sec = (double)end.tv_sec*1000000000.0 + (double)end.tv_nsec;
+
+  if (end_sec < start_sec) {
+    return 0;
+  } else {
+    return end_sec - start_sec;
+  }
+}
+
+int main (int argc, char const *argv[]) { 
+  try {
+    rain_time = std::stoi(argv[1]);
+    absorb_rate = std::stod(argv[2]);
+    N = std::stoi(argv[3]);
+  }
+  catch (std::exception& e) 
+    {
+      std::cout << e.what() << std::endl;
+    }
+  std::string file_name = argv[4];
+  std::ifstream file(file_name);
+  std::string line = "";
+  init();
+  int row = 0;
+  while (!file.eof()) {
+    std::getline(file, line);
+    int col = 0;
+    std::string buf;
+    std::stringstream ss(line);
+    while (ss >> buf) {
+      try 
+	{
+	  land[row][col] = std::stoi(buf);
+	}
+      catch (std::exception e) 
+	{
+	  std::cout << e.what() << std::endl;
+	}
+      ++col;
+    }
+    ++row;
+  }
+  init_lowest();
+  int timestep = 0;
+
+  struct timespec start_time, end_time;
+  clock_gettime(CLOCK_MONOTONIC, &start_time);
+
+  int total_timestep = rain(t, rain_time, timestep);
+
+  clock_gettime(CLOCK_MONOTONIC, &end_time);
+  double elapsed_s = calc_time(start_time, end_time) / 1000000000.0;
+  std::cout << "running time for " << N << "is "<< elapsed_s << std::endl;
+
+  std::cout << "Rainfall simulation took "  << total_timestep << " time steps to complete." << std::endl;
+  std::cout << "The following grid shows the number of raindrops absorbed at each point: " << std::endl;
+  for (std::vector<double> row : absorbed) {
+    for (double value : row) {
+      std::cout << std::setw(6) << std::right << value << " ";
+    }
+    std::cout << std::endl;
+  }
+  return 0;
+}
